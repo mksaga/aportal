a7e0c43d5126d178668add2975afe77911f2f507
Mohamed Aly <mohamed.aly06@gmail.com>
2016-03-02 09:38:17 -0600

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+
   def new
+  	@user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+    # to see detailed information about variables, etc., decomment below:
+    # debugger
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the IST Portal!"
+      redirect_to @user # equivalent to: redirect_to user_url(@user)
+    else
+      # Show the signup form again
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                        :password_confirmation)
+    end
+
 end
